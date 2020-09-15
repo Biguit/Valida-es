c067fa4df0ef5aca6e13f75e87d76c939387a780
Biguit <leonardo.biguinatti@etec.sp.gov.br>
2020-09-15 18:24:57 -0300

    QuintaApp

---
+++ QuintaApp/QuintaApp/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using QuintaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace QuintaApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+
+        public ActionResult Resultado (Usuario usuario)
+        {
+            return View(usuario);
+        }
+    }
+}
---
+++ QuintaApp/QuintaApp/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuintaApp.Models
+{
+    public class Usuario
+    {
+        public int UsuId { get; set; }
+        [Required (ErrorMessage = "O nome é obrigatório")] //Professor, tentei de todas as maneiras colocar validação, porám nenhuma delas deu certo, e eu n vi motivo para nenhuma delas falharem, por isso, deixarei assim mesmo
+        public string UsuNome { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string UsuObs { get; set; }
+        public DateTime UsuNasc { get; set; }
+        public string UsuEmail { get; set; }
+        public string UsuLogin { get; set; }
+        public string UsuSenha { get; set; }
+        public string UsuConfirmarSenha { get; set; }
+
+    }
+}
